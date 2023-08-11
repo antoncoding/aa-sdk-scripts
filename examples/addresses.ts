b646c3bca0452a6e0794ec29e04c2aa4d1f1c290
Anton Cheng <antonassocareer@gmail.com>
2023-08-11 12:02:37 +0800

    chore: cleanup, rename scripts to relay-lyra-wrapper.ts and relay-safe.ts

---
+++ examples/addresses.ts
@@ -0,0 +1,8 @@
+export const addresses = {
+  goerli: {
+    l1USDC: '0x07865c6e87b9f70255377e024ace6630c1eaa37f',
+    l2USDC: '0x7E07E15D2a87A24492740D16f5bdF58c16db0c4E',
+    l1StandardBridge: '0x636Af16bf2f682dD3109e60102b8E1A089FedAa8',
+    lyraForwarder: '0x1dC3c8f65529E32626bbbb901cb743d373a7193e'
+  },
+}
--- examples/l1-deposit/relay-lyra-wrapper.ts
+++ examples/l1-deposit/relay-lyra-wrapper.ts
@@ -1,44 +1,37 @@
 import { ethers } from 'ethers'
 import { GelatoRelay, CallWithERC2771Request, ERC2771Type } from "@gelatonetwork/relay-sdk";
-const relay = new GelatoRelay();
+import { addresses } from '../addresses';
+import {signPermit} from "../permit"
+import forwarderAbi from "../abi/lyra-forwarder.json";
 
 // dot env
 import dotenv from 'dotenv'
 dotenv.config()
 
-// import l1BridgeAbi from "../abi/L1StandardBridgeProxy.json";
-// import usdcAbi from "../abi/usdc.json";
-import forwarderAbi from "../abi/lyra-forwarder.json";
-
-import {signPermit} from "../permit"
-
 const OWNER_PK = process.env.OWNER_PRIVATE_KEY!
-
-const RPC_URL = 'https://goerli.infura.io/v3/26251a7744c548a3adbc17880fc70764'
-const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
-
+const RPC_URL = process.env.RPC_URL!;
 const GELATO_RELAY_API_KEY = process.env.GELATO_RELAY_API_KEY!;
 
+const networkConfig = addresses.goerli
+
 // This will be connected wallet in Metamask
+const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
 const user = new ethers.Wallet(OWNER_PK, provider);
 
-// network configs
-const mainnetUSDC = '0x07865c6e87b9f70255377e024ace6630c1eaa37f' // goerli
-const lyraForwarder = '0x1dC3c8f65529E32626bbbb901cb743d373a7193e'
-
-const l2Safe = '0xe3b35100706f86257372478a2D41C2f317E16c5f'
+const relay = new GelatoRelay();
 
 // contract instances
-const forwarder = new ethers.Contract(lyraForwarder, forwarderAbi, user);
+const forwarder = new ethers.Contract(networkConfig.lyraForwarder, forwarderAbi, user);
 
 async function relayTransaction() {
-  
   const depositAmount = '77000000';
 
-  
+  // calculate L2 safe address to deposit in, or it can be any recipient
+  const l2Safe = '0xe3b35100706f86257372478a2D41C2f317E16c5f'
+
   // Build Permit data
   const deadline = Math.floor(Date.now() / 1000) + 86400;
-  const permitData = await signPermit(user, mainnetUSDC, lyraForwarder,  depositAmount, deadline)
+  const permitData = await signPermit(user, networkConfig.l1USDC, networkConfig.l2USDC,  depositAmount, deadline)
   
   // whole tx
   const minGas = '200000'
@@ -53,19 +46,17 @@ async function relayTransaction() {
   // Populate a relay request
   const request: CallWithERC2771Request = {
     chainId: provider.network.chainId,
-    target: lyraForwarder,
+    target: networkConfig.lyraForwarder,
     data: ethers.utils.hexlify(data as string),
     user: user.address
   };
 
   const sigData = await relay.getSignatureDataERC2771(request, user, ERC2771Type.SponsoredCall);
 
-  console.log("sigData.struct", sigData.struct);
-
   const relayResponse = await relay.sponsoredCallERC2771WithSignature(sigData.struct, sigData.signature, GELATO_RELAY_API_KEY);  
-  
-  console.log({ relayResponse })
   
   console.log(`https://relay.gelato.digital/tasks/status/${relayResponse.taskId} `);
 }
+
+
 relayTransaction();
