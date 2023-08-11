6f0348789fc5dbb15ec2f952282233679b2780d1
Anton Cheng <antonassocareer@gmail.com>
2023-08-11 16:23:47 +0800

    docs: update readme

--- contracts/LyraForwarder.sol
+++ contracts/LyraForwarder.sol
@@ -8,12 +8,19 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {IStandardBridge} from "./interfaces/IStandardBridge.sol";
 
+/**
+ * @title LyraForwarder
+ * @notice this contract help onboarding users with only USDC in their wallet to our custom rollup, with help of Gelato Relayer
+ */
 contract LyraForwarder is ERC2771Context {
 
+    ///@dev L1 USDC address.
     address immutable public usdcLocal;
 
+    ///@dev L2 USDC address. 
     address immutable public usdcRemote;
 
+    ///@dev L1StandardBridge address.
     address immutable public bridge;
 
     struct PermitData {
@@ -25,7 +32,7 @@ contract LyraForwarder is ERC2771Context {
     }
     
     /**
-     * @param _trustedForwarder GelatoRelay1BalanceERC2771 forwarder (0xd8253782c45a12053594b9deB72d8e8aB2Fca54c)
+     * @param _trustedForwarder GelatoRelay1BalanceERC2771 forwarder (0xd8253782c45a12053594b9deB72d8e8aB2Fca54c) for all non-zkSync-EVM
      **/
     constructor(address _trustedForwarder, address _usdcLocal, address _usdcRemote,  address _bridge) ERC2771Context(_trustedForwarder) {
         usdcLocal = _usdcLocal;
@@ -35,6 +42,13 @@ contract LyraForwarder is ERC2771Context {
         IERC20(_usdcLocal).approve(bridge, type(uint).max);
     }
 
+    /**
+     * @notice Deposit USDC to L2
+     * @dev This function use _msgSender() to be compatible with ERC2771. 
+     *      Users can either interact directly with this contract (to do permit + deposit in one go),
+     *      or sign a Gelato relay request, and let the GelatoRelay1BalanceERC2771 contract forward the call to this contract.
+     *      With the latter, _msgSender() will be the signer which is verified by GelatoRelay1BalanceERC2771
+     */
     function forwardUSDCToL2(PermitData calldata permit, uint256 depositAmount, address l2Receiver, uint32 minGasPrice) external {
         // step 1 (optional) call permit
         if (permit.value != 0) {
@@ -44,10 +58,7 @@ contract LyraForwarder is ERC2771Context {
         // step 2: transferFrom msg.sender to this contract
         IERC20(usdcLocal).transferFrom(_msgSender(), address(this), depositAmount);
 
-        // step 3: calculate target SafeAddress (todo: is this necessary?)
-
-
-        // step 4: call bridge to L2
+        // step 3: call bridge to L2
         IStandardBridge(bridge).bridgeERC20To(usdcLocal, usdcRemote, l2Receiver,  depositAmount, minGasPrice, "");
     }
 }
