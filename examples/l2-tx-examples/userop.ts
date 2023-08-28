5f8a0c617976a967bc494147995b5e641a240a28
Anton Cheng <antonassocareer@gmail.com>
2023-08-28 18:27:51 +0800

    build: tx success with higher gas limit

--- examples/l2-tx-examples/userop.ts
+++ examples/l2-tx-examples/userop.ts
@@ -1,11 +1,13 @@
 import { ethers, BigNumber } from "ethers";
-import { addresses } from '../addresses';
 
 import { Client, Presets } from "userop";
 import { BundlerJsonRpcProvider, IUserOperationMiddlewareCtx } from "userop";
 
 import dotenv from 'dotenv'
 dotenv.config()
+
+import usdcAbi from "../abi/usdc.json";
+
 
 const privateKey = process.env.OWNER_PRIVATE_KEY!
 const bundlerUrl = process.env.LOCAL_BUNDLER_URL!
@@ -14,20 +16,24 @@ const entryPoint = process.env.LOCAL_ENTRY_POINT!
 const simpleAccountFactory = process.env.LOCAL_SIMPLEACCOUNT_FACTORY
 
 const rpcUrl = process.env.L2_RPC!; // Lyra staging
-// const nodeProvider = new ethers.providers.JsonRpcProvider(rpcUrl)
 const provider = new BundlerJsonRpcProvider(rpcUrl).setBundlerRpc(bundlerUrl);
 
 const user = new ethers.Wallet(privateKey, provider);
 
-const networkConfig = addresses.goerli
-
 const dumbPaymaster = "0xd198a6f2B3D07a03161FAB8006502e911e5c548e";
+const stagingUSDC = "0xAeE02dB1c65ce17211252f7eba0CDCcA07E95548"
+
+// contract instances
+const usdcContract = new ethers.Contract(stagingUSDC, usdcAbi, user);
 
 // apply our own dumb paymaster: pay for anyone
 const paymasterMiddleware: (context: IUserOperationMiddlewareCtx) => Promise<void> = async (context) => {
   context.op.paymasterAndData = dumbPaymaster;
   // previously only 21000, need to add more (adding 30000)
   context.op.preVerificationGas = BigNumber.from(context.op.preVerificationGas).add(BigNumber.from(30000));
+
+  // need to update callGasLimit as well
+  context.op.callGasLimit = BigNumber.from(context.op.callGasLimit).add(BigNumber.from(60000));
 }
 
 async function transferWithUserOp(transferAmount: string) {
@@ -48,12 +54,15 @@ async function transferWithUserOp(transferAmount: string) {
 
   const client = await Client.init(rpcUrl, {overrideBundlerRpc: bundlerUrl, entryPoint: entryPoint});
 
-  // build transaction: USDC transfer for now
-  const target = networkConfig.l1USDC as `0x${string}`;
-  const value = BigInt(transferAmount);
+  // build transaction: random send usdc tx
+  const target = stagingUSDC; // usdc address
+  const data =  usdcContract.interface.encodeFunctionData("transfer(address,uint256)", [
+    user.address, // owner / can be random
+    transferAmount,
+  ])
 
   const res = await client.sendUserOperation(
-    simpleAccount.execute(target, value, "0x"),
+    simpleAccount.execute(target, 0, data),
     { 
       onBuild: (op) => console.log("Signed UserOperation:", op.sender),
     }
