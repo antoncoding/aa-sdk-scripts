68c03ae3eeff284faeebc67cd513191e19aed6c4
Anton Cheng <antonassocareer@gmail.com>
2023-09-25 10:38:28 +0800

    chore: script for setting paymaster in staging

--- scripts/deploy-paymaster.ts
+++ scripts/deploy-paymaster.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import DumbPaymaster from "../artifacts/contracts/DumbPaymaster.sol/DumbPaymaster.json";
+import SignaturePaymaster from "../artifacts/contracts/SignaturePaymaster.sol/SignaturePaymaster.json";
 
 const deployerPK = process.env.SPONSOR_PRIVATE_KEY!
 const provider = new ethers.providers.JsonRpcProvider(process.env.L2_RPC!)
@@ -9,10 +9,12 @@ async function main() {
 
   const entryPoint = '0x33a07c35557De1e916B26a049e1165D47d462f6B'
   
-  const factory = new ethers.ContractFactory(DumbPaymaster.abi, DumbPaymaster.bytecode, deployer)
+  const factory = new ethers.ContractFactory(SignaturePaymaster.abi, SignaturePaymaster.bytecode, deployer)
   const paymaster = await factory.deploy(entryPoint);
 
-  console.log(`DumbPaymaster deployed to ${paymaster.address}`);
+  console.log(`Signature Paymaster deployed to ${paymaster.address}`);
+
+  paymaster.setSigner("0x6666fe8F577F202Ec729BF653ec25Af5403cbd76", true)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
