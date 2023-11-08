0ac0de4eefbdedb34300ded3f8bc454a8ef263de
Anton Cheng <antonassocareer@gmail.com>
2023-11-08 17:59:15 +0800

    chore: remove unused files

--- contracts/interfaces/IStandardBridge.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-interface IStandardBridge {
-    function bridgeERC20To(address localToken, address remoteToken, address to, uint256 amount, uint32 minGasPrice, bytes calldata data) external;
-}
