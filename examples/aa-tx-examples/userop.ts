05e4f3ecd5c3eec325ce2f639660e82ec73357e6
Anton Cheng <antonassocareer@gmail.com>
2023-12-24 14:21:24 +0800

    build: userOp script run on mainnet

--- examples/aa-tx-examples/userop.ts
+++ examples/aa-tx-examples/userop.ts
@@ -17,54 +17,53 @@ const provider = new BundlerJsonRpcProvider(rpcUrl).setBundlerRpc(bundlerUrl);
 
 const user = new ethers.Wallet(privateKey, provider);
 
-// Our l2 testnet
+// Lyra L2 Mainnet configs
 
-const entryPoint = '0x33a07c35557De1e916B26a049e1165D47d462f6B'!
-const simpleAccountFactory = '0x7E072a60c7297bD9d027B2a43cD0C27559aF2f58'
-const dumbPaymaster = "0xd198a6f2B3D07a03161FAB8006502e911e5c548e";
-const stagingUSDC = "0xAeE02dB1c65ce17211252f7eba0CDCcA07E95548"
-
-// contract instances
-const usdcContract = new ethers.Contract(stagingUSDC, usdcAbi, user);
+const entryPoint = '0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789'!
+const lightAccountFactory = '0x000000893A26168158fbeaDD9335Be5bC96592E2'
+const usdc = '0x6879287835A86F50f784313dBEd5E5cCC5bb8481'
+const usdcContract = new ethers.Contract(usdc, usdcAbi, user);
 
 // apply our own dumb paymaster: pay for anyone
-const paymasterMiddleware: (context: IUserOperationMiddlewareCtx) => Promise<void> = async (context) => {
-  context.op.paymasterAndData = dumbPaymaster;
-  // previously only 21000, need to add more (adding 30000)
-  context.op.preVerificationGas = BigNumber.from(context.op.preVerificationGas).add(BigNumber.from(30000));
+// const paymasterMiddleware: (context: IUserOperationMiddlewareCtx) => Promise<void> = async (context) => {
+//   context.op.paymasterAndData = '0x'
+//   // previously only 21000, need to add more (adding 30000)
+//   context.op.preVerificationGas = BigNumber.from(context.op.preVerificationGas).add(BigNumber.from(30000));
 
-  // need to update callGasLimit as well
-  context.op.callGasLimit = BigNumber.from(context.op.callGasLimit).add(BigNumber.from(60000));
-}
+//   // need to update callGasLimit as well
+//   context.op.callGasLimit = BigNumber.from(context.op.callGasLimit).add(BigNumber.from(60000));
+// }
 
-// Not working properly, this is only a script running on our staging env
-async function run(transferAmount: string) {
+// Send directly from smart wallet. Needs ETH (without paymaster middleware)
+async function run() {
   // simpleAccount preset
   const simpleAccount = await Presets.Builder.SimpleAccount.init(
     user, // Any object compatible with ethers.Signer
     rpcUrl,
     {
       entryPoint: entryPoint,
-      factory: simpleAccountFactory,
+      factory: lightAccountFactory,
       overrideBundlerRpc: bundlerUrl,
-      paymasterMiddleware: paymasterMiddleware
+      // paymasterMiddleware: paymasterMiddleware
     }
   );
 
   const sender = simpleAccount.getSender();
   console.log('sender (smart contract wallet)', sender)
 
+  const balance = await usdcContract.balanceOf(sender)
+  console.log(`LightAcc USDC balance:\t${ethers.utils.formatUnits(balance, 6)}`)
+
   const client = await Client.init(rpcUrl, {overrideBundlerRpc: bundlerUrl, entryPoint: entryPoint});
 
   // build transaction: random send usdc tx
-  const target = stagingUSDC; // usdc address
   const data =  usdcContract.interface.encodeFunctionData("transfer(address,uint256)", [
-    user.address, // owner / can be random
-    transferAmount,
+    '0xaa', // anyone
+    balance,
   ])
 
   const res = await client.sendUserOperation(
-    simpleAccount.execute(target, 0, data),
+    simpleAccount.execute(usdc, 0, data),
     { 
       onBuild: (op) => console.log("Signed UserOperation:", op.sender),
     }
@@ -77,4 +76,4 @@ async function run(transferAmount: string) {
   result?.transactionHash && console.log(`Transaction hash: ${result.transactionHash}`);
 }
 
-run('1000000').then(() => process.exit(0))
+run().then(() => process.exit(0))
